864bb1ca16ee8c1005db9dc7dcbe623392d65d25
gggiz <2409438419@qq>
2026-06-15 14:37:11 +0800

    Hell Abyss: custom tool material, 20 attack damage

--- src/main/java/com/albedo/item/HellAbyssItem.java
+++ src/main/java/com/albedo/item/HellAbyssItem.java
@@ -1,17 +1,27 @@
 package com.albedo.item;
 
 import net.minecraft.network.chat.Component;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.world.item.AxeItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.ToolMaterial;
-import net.minecraft.world.item.component.ItemAttributeModifiers;
 
 public class HellAbyssItem extends AxeItem {
-    private static final float ATTACK_DAMAGE = 10.0f;
-    private static final float ATTACK_SPEED = -3.0f;
+    private static final float ATTACK_DAMAGE = 14.0f;
+    private static final float ATTACK_SPEED = -2.8f;
+
+    private static final ToolMaterial HELL_ABYSS_MATERIAL = new ToolMaterial(
+            BlockTags.INCORRECT_FOR_NETHERITE_TOOL,
+            2500,
+            10.0f,
+            5.0f,
+            22,
+            ItemTags.NETHERITE_TOOL_MATERIALS
+    );
 
     public HellAbyssItem(Properties properties) {
-        super(ToolMaterial.NETHERITE, ATTACK_DAMAGE, ATTACK_SPEED, properties);
+        super(HELL_ABYSS_MATERIAL, ATTACK_DAMAGE, ATTACK_SPEED, properties);
     }
 
     @Override
